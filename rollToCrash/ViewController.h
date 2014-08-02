1773c7204e927932abb2625d3e219e0f0c1da1e6
七野祐太 <shichinoyuta@shichinoyuta-no-MacBook-Air.local>
2014-08-02 15:20:58 +0900

    追加 #import "ImageViewCircle.h"

--- rollToCrash/ViewController.h
+++ rollToCrash/ViewController.h
@@ -13,6 +13,7 @@
 #import "NADView.h"
 #define MY_BANNER_UNIT_ID @"ca-app-pub-5959590649595305/5220821270"
 #define MY_INTERSTITIAL_UNIT_ID @"ca-app-pub-5959590649595305/4941619672"
+#import "ImageViewCircle.h"
 
 @interface ViewController : UIViewController<GADBannerViewDelegate,GADInterstitialDelegate,NADViewDelegate>{
     //【Ad】AdMobバナー：インスタンス変数として1つ宣言
