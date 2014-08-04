663e2c2a43e3aab0d26902719839c83ea210d8fb
七野祐太 <shichinoyuta@shichinoyuta-no-MacBook-Air.local>
2014-08-04 17:12:29 +0900

    + (void)appearWithRotate:(UIButton *)btn ９０度回転までに大きさ１倍それから残り3/4回展するように変更 アルファは適当に調整要

--- rollToCrash/UIButtonAnimation.m
+++ rollToCrash/UIButtonAnimation.m
@@ -14,61 +14,52 @@
 
 // pauseBtnの出現アニメーション
 + (void)appearWithRotate:(UIButton *)btn{
-    btn.imageView.alpha = 0;
+    btn.imageView.transform = CGAffineTransformIdentity;
+
     btn.hidden = 0;
+    btn.alpha = 0;
     btn.imageView.transform = CGAffineTransformMakeScale(0.01, 0.01);
     
-    CGAffineTransform t1 = CGAffineTransformMakeRotation(M_PI/2.0f);
-    CGAffineTransform t2 = CGAffineTransformRotate(t1, M_PI/2.0f);
-    CGAffineTransform t3 = CGAffineTransformRotate(t2, M_PI/2.0f);
-    CGAffineTransform t4 = CGAffineTransformRotate(t3, M_PI/2.0f);
+    float scale = 1.0;
+    CGAffineTransform t1 = CGAffineTransformMakeRotation(M_PI_2); // M_PI 180度 M_PI_2 90度
+    CGAffineTransform t5 = CGAffineTransformMakeScale(scale, scale);
+  
     
-    CGAffineTransform t5 = CGAffineTransformMakeScale(1.25, 1.25);
-    CGAffineTransform t6 = CGAffineTransformScale(t5, 1.25, 1.25);
-    CGAffineTransform t7 = CGAffineTransformScale(t6, 1.25, 1.25);
-    CGAffineTransform t8 = CGAffineTransformScale(t7, 1.25, 1.25);
-    
-    
-    
-    [UIView animateWithDuration:0.12f
+    [UIView animateWithDuration:0.1f
                           delay:0.0f
-                        options:UIViewAnimationOptionCurveEaseIn
+                        options:UIViewAnimationOptionCurveLinear
                      animations:^{
-                         btn.imageView.alpha = 0.3;
+                         btn.alpha = 0.5;
                          btn.imageView.transform =  CGAffineTransformConcat(t1, t5);
                      }
                      completion:^(BOOL finished){
-                         [UIView animateWithDuration:0.12f
+                         [UIView animateWithDuration:0.1f
                                                delay:0.0f
                                              options:UIViewAnimationOptionCurveLinear
                                           animations:^{
-                                              btn.imageView.alpha = 0.6;
-                                              btn.imageView.transform =  CGAffineTransformConcat(t2, t6);
+                                              btn.alpha = 0.6;
+                                               btn.imageView.transform =  CGAffineTransformConcat(btn.imageView.transform,t1);
                                           }
                                           completion:^(BOOL finished){
-                                              [UIView animateWithDuration:0.12f
-                                                                    delay:0.0f
+                                              [UIView animateWithDuration:0.1f
+                                                                    delay:0.0
                                                                   options:UIViewAnimationOptionCurveLinear
                                                                animations:^{
-                                                                   btn.imageView.alpha = 1;
-                                                                   btn.imageView.transform =  CGAffineTransformConcat(t3, t7);
-                                                               }
-                                                               completion:^(BOOL finished){
-                                                                   [UIView animateWithDuration:0.12f
+                                                                   btn.alpha = 0.75;
+                                                                   btn.imageView.transform =  CGAffineTransformConcat(btn.imageView.transform,t1);
+                                                               }completion:^(BOOL finished) {
+                                                                   [UIView animateWithDuration:0.1f
                                                                                          delay:0.0f
-                                                                                       options:UIViewAnimationOptionCurveEaseOut
-                                                                                    animations:^{
-                                                                                        btn.imageView.alpha = 1;
-                                                                                        btn.imageView.transform =  CGAffineTransformConcat(t4, t8);
-                                                                                        // アニメーションが再生されるまでボタンを無効化
-                                                                                        [btn setEnabled:1];
-                                                                                    }
-                                                                                    completion:nil];
-                                                                   
+                                                                                       options:UIViewAnimationOptionCurveEaseOut animations:^{
+                                                                                           btn.alpha = 1;
+                                                                                           btn.imageView.transform =  CGAffineTransformConcat(btn.imageView.transform,t1);
+                                                                                       }completion:^(BOOL finished) {
+                                                                                           [btn setEnabled:1];
+                                                                                       }];
                                                                }];
-                                              
                                           }];
                      }];
+     
     
 }
 
